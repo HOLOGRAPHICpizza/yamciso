1a888e8c25166adeacfbd5f635dcaed195dade51
hello45044 <hello45044@db374bbc-19c2-11df-b1cb-2be540f5faec>
2010-02-15 22:02:17 +0000

    Reduces calls to java library for massive speed increase. v1.3

--- YaMcIso/Program.cs
+++ YaMcIso/Program.cs
@@ -51,59 +51,68 @@ namespace YaMcIso
                 return;
             }
 
-            Console.WriteLine("Rendering...");
+            Console.WriteLine("Loading Level...");
 
             Level lvl = Level.Load(CommandConfig.MapFile);
-
-            System.Drawing.Bitmap flag = new System.Drawing.Bitmap((1024 + 128) * 2, (1500 + 64) * 2);
 
             int lvlwidth = lvl.width;
             int lvldepth = lvl.depth;
             int lvlheight = lvl.height;
 
+			// Copy level into array for better performance
+			fTile[,,] lvlArray = new fTile[lvlwidth,lvldepth,lvlheight];
+			for (int x = 0; x < lvlwidth; x++)
+			{
+				for (int y = 0; y < lvldepth; y++)
+				{
+					for (int z = 0; z < lvlheight; z++)
+					{
+						lvlArray[x, y, z] = new fTile(lvl.GetTile(x,y,z));
+					}
+				}
+
+				Console.WriteLine("Loaded " + (x + 1) + "/" + lvlwidth);
+			}
+
+			// Begin drawing the image.
+			Console.WriteLine("\nRendering Level...");
+			System.Drawing.Bitmap flag = new System.Drawing.Bitmap((1024 + 128) * 2, (1500 + 64) * 2);
+			Boolean Xclear = true;
+			Boolean Yclear = true;
+			Boolean Zclear = true;
             for (int x = 0; x < lvlwidth; x++)
             {
                 for (int y = 0; y < lvldepth; y++)
                 {
                     for (int z = 0; z < lvlheight; z++)
                     {
-                        if (lvl.GetTile(x, y, z).ToString() != "Air")
+						if (!lvlArray[x, y, z].isAir)
                         {
-                            if (checkValidBlock(lvl.GetTile(x + 1, y, z)) ||
-                                checkValidBlock(lvl.GetTile(x, y + 1, z)) ||
-                                checkValidBlock(lvl.GetTile(x, y, z + 1)))
+
+							Xclear = true;
+							Yclear = true;
+							Zclear = true;
+							if (x + 1 < lvlwidth) { Xclear = lvlArray[x + 1, y, z].transparent; }
+							if (y + 1 < lvldepth) { Yclear = lvlArray[x, y + 1, z].transparent; }
+							if (z + 1 < lvlheight) { Zclear = lvlArray[x, y, z + 1].transparent; }
+
+                            if ( Xclear || Yclear || Zclear )
                             {
-                                drawBlock(flag, x, z, y, lvl.GetTile(x, y, z));
+								drawBlock(flag, x, z, y, lvlArray[x, y, z]);
                             }
                         }
                     }
                 }
 
-                Console.WriteLine("Slice " + (x+1) + "/" + lvlwidth);
+				Console.WriteLine("Rendered " + (x + 1) + "/" + lvlwidth);
             }
 
             flag.Save(CommandConfig.Output, System.Drawing.Imaging.ImageFormat.Png);
 
-            Console.WriteLine("Processing image...");
+            Console.WriteLine("\nProcessing image...");
             processImage(CommandConfig.Output, CommandConfig.ImgWidth);
 
             Console.WriteLine(CommandConfig.Output + " rendered successfully!");
-        }
-
-        static bool checkValidBlock(Tile tile)
-        {
-            if (tile.ToString() == "Air" ||
-                tile.ToString() == "Water" ||
-                tile.ToString() == "Glass" ||
-                tile.ToString() == "StillWater"
-                )
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
         }
 
         static void processImage(String file, int width)
@@ -123,214 +132,21 @@ namespace YaMcIso
             pro.WaitForExit();
         }
 
-        static void drawBlock(Bitmap flag, int x, int y, int z, Tile Tile)
+        static void drawBlock(Bitmap flag, int x, int y, int z, fTile til)
         {
-            Color Main;
-            Color Highlight;
-            Color Shadow;
-            Boolean renderGlass = false;
-            Boolean renderWater = false;
-            Main = Color.FromArgb(128, 128, 128);
-            Highlight = Color.FromArgb(164, 164, 164);
-            Shadow = Color.FromArgb(100, 100, 100);
-            switch (Tile)
-            {
-                case Tile.Dirt:
-                    Main = Color.FromArgb(147, 96, 0);
-                    Highlight = Color.Sienna;
-                    Shadow = Color.SaddleBrown;
-                    break;
-
-                case Tile.Grass:
-                    Main = Color.Green;
-                    Highlight = Color.Sienna;
-                    Shadow = Color.SaddleBrown;
-                    break;
-
-                case Tile.Stone:
-                    Main = Color.FromArgb(128, 128, 128);
-                    Highlight = Color.FromArgb(164, 164, 164);
-                    Shadow = Color.FromArgb(100, 100, 100);
-                    break;
-
-                case Tile.Cobblestone:
-                    Main = Color.FromArgb(90, 90, 90);
-                    Highlight = Color.FromArgb(124, 124, 124);
-                    Shadow = Color.FromArgb(64, 64, 64);
-                    break;
-
-                case Tile.Water:
-                    Main = Color.FromArgb(0, 171, 236);
-                    Highlight = Color.FromArgb(70, 204, 255);
-                    Shadow = Color.FromArgb(0, 123, 170);
-                    renderWater = true;
-                    break;
-
-                case Tile.StillWater:
-                    Main = Color.FromArgb(0, 171, 236);
-                    Highlight = Color.FromArgb(70, 204, 255);
-                    Shadow = Color.FromArgb(0, 123, 170);
-                    renderWater = true;
-                    break;
-
-                case Tile.Lava:
-                    Main = Color.FromArgb(255, 125, 0);
-                    Highlight = Color.FromArgb(255, 100, 0);
-                    Shadow = Color.FromArgb(170, 50, 0);
-                    break;
-
-                case Tile.StillLava:
-                    Main = Color.FromArgb(236, 50, 0);
-                    Highlight = Color.FromArgb(255, 100, 25);
-                    Shadow = Color.FromArgb(170, 0, 0);
-                    break;
-
-                /*case Tile.StillLava:
-                Main = Color.FromArgb(236, 171, 0);
-                Highlight = Color.FromArgb(255, 204, 70);
-                Shadow = Color.FromArgb(170, 123, 0);
-                break;*/
-
-                case Tile.Glass:
-                    Main = Color.FromArgb(255, 255, 255);
-                    Highlight = Color.FromArgb(255, 255, 255);
-                    Shadow = Color.FromArgb(255, 255, 255);
-                    renderGlass = true;
-                    break;
-
-                case Tile.SolidGold:
-                    Main = Color.FromArgb(238, 237, 120);
-                    Highlight = Color.FromArgb(238, 225, 64);
-                    Shadow = Color.FromArgb(217, 202, 19);
-                    break;
-
-                case Tile.Sand:
-                    Main = Color.FromArgb(208, 206, 121);
-                    Highlight = Color.FromArgb(217, 213, 157);
-                    Shadow = Color.FromArgb(197, 190, 109);
-                    break;
-
-                case Tile.Gravel:
-                    Main = Color.FromArgb(158, 156, 71);
-                    Highlight = Color.FromArgb(117, 113, 57);
-                    Shadow = Color.FromArgb(97, 90, 09);
-                    break;
-
-                case Tile.Red:
-                    Main = Color.FromArgb(255, 0, 0);
-                    Highlight = Color.FromArgb(255, 50, 50);
-                    Shadow = Color.FromArgb(205, 0, 0);
-                    break;
-
-                case Tile.Orange:
-                    Main = Color.FromArgb(255, 128, 0);
-                    Highlight = Color.FromArgb(255, 178, 50);
-                    Shadow = Color.FromArgb(205, 78, 0);
-                    break;
-
-                case Tile.Yellow:
-                    Main = Color.FromArgb(255, 255, 0);
-                    Highlight = Color.FromArgb(255, 255, 50);
-                    Shadow = Color.FromArgb(205, 205, 0);
-                    break;
-
-                case Tile.LightGreen:
-                    Main = Color.FromArgb(128, 255, 0);
-                    Highlight = Color.FromArgb(178, 255, 50);
-                    Shadow = Color.FromArgb(78, 205, 0);
-                    break;
-
-                case Tile.Green:
-                    Main = Color.FromArgb(0, 255, 0);
-                    Highlight = Color.FromArgb(50, 255, 50);
-                    Shadow = Color.FromArgb(0, 205, 0);
-                    break;
-
-                case Tile.AquaGreen:
-                    Main = Color.FromArgb(0, 255, 128);
-                    Highlight = Color.FromArgb(50, 255, 178);
-                    Shadow = Color.FromArgb(0, 205, 78);
-                    break;
-
-                case Tile.Cyan:
-                    Main = Color.FromArgb(0, 128, 255);
-                    Highlight = Color.FromArgb(50, 178, 255);
-                    Shadow = Color.FromArgb(0, 78, 205);
-                    break;
-
-                case Tile.Blue:
-                    Main = Color.FromArgb(0, 0, 255);
-                    Highlight = Color.FromArgb(50, 50, 205);
-                    Shadow = Color.FromArgb(0, 0, 205);
-                    break;
-
-                case Tile.Indigo:
-                    Main = Color.FromArgb(128, 0, 255);
-                    Highlight = Color.FromArgb(178, 50, 255);
-                    Shadow = Color.FromArgb(78, 0, 205);
-                    break;
-
-                case Tile.Purple:
-                    Main = Color.FromArgb(255, 0, 255);
-                    Highlight = Color.FromArgb(255, 50, 255);
-                    Shadow = Color.FromArgb(205, 0, 205);
-                    break;
-
-                case Tile.Pink:
-                    Main = Color.FromArgb(255, 0, 128);
-                    Highlight = Color.FromArgb(255, 50, 178);
-                    Shadow = Color.FromArgb(205, 0, 78);
-                    break;
-
-                case Tile.Bedrock:
-                    Main = Color.FromArgb(50, 50, 50);
-                    Highlight = Color.FromArgb(100, 100, 100);
-                    Shadow = Color.FromArgb(25, 25, 25);
-                    break;
-
-                case Tile.DarkGrey:
-                    Main = Color.FromArgb(65, 65, 65);
-                    Highlight = Color.FromArgb(115, 115, 115);
-                    Shadow = Color.FromArgb(40, 40, 40);
-                    break;
-
-                case Tile.LightGrey:
-                    Main = Color.FromArgb(125, 125, 125);
-                    Highlight = Color.FromArgb(175, 175, 175);
-                    Shadow = Color.FromArgb(75, 75, 75);
-                    break;
-
-                case Tile.White:
-                    Main = Color.FromArgb(200, 200, 200);
-                    Highlight = Color.FromArgb(255, 255, 255);
-                    Shadow = Color.FromArgb(150, 150, 150);
-                    break;
-
-                case Tile.Trunk:
-                    Main = Color.FromArgb(147, 96, 0);
-                    Highlight = Color.Sienna;
-                    Shadow = Color.SaddleBrown;
-                    break;
-
-                case Tile.Leaves:
-                    Main = Color.FromArgb(0, 128, 0);
-                    Highlight = Color.FromArgb(50, 178, 0);
-                    Shadow = Color.FromArgb(0, 78, 0);
-                    break;
-            }
-            int xOffset = (flag.Width / 4);
+			int xOffset = (flag.Width / 4);
             int yOffset = (flag.Height / 2);
-            if (renderGlass)
+            if (til.renderGlass)
             {
                 drawGlass(flag, (x + xOffset - y), (x + yOffset + y), z);
             }
-            else if (renderWater)
+            else if (til.renderWater)
             {
                 drawWater(flag, (x + xOffset - y), (x + yOffset + y), z);
             }
             else
             {
-                drawCube(flag, (x + xOffset - y), (x + yOffset + y), z, Main, Highlight, Shadow);
+                drawCube(flag, (x + xOffset - y), (x + yOffset + y), z, til.Main, til.Highlight, til.Shadow);
             }
         }
 
---
+++ YaMcIso/fTile.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using com.mojang.minecraft.level;
+using System.Drawing;
+
+namespace YaMcIso
+{
+	class fTile
+	{
+		public Color Main = Color.FromArgb(128, 128, 128);
+        public Color Highlight = Color.FromArgb(164, 164, 164);
+        public Color Shadow = Color.FromArgb(100, 100, 100);
+		public Boolean renderGlass = false;
+		public Boolean renderWater = false;
+		public Boolean transparent = false;
+		public Boolean isAir = false;
+
+		public fTile(Tile til)
+		{
+			switch (til)
+			{
+				case Tile.Air:
+					transparent = true;
+					isAir = true;
+					break;
+
+				case Tile.Dirt:
+					Main = Color.FromArgb(147, 96, 0);
+					Highlight = Color.Sienna;
+					Shadow = Color.SaddleBrown;
+					break;
+
+				case Tile.Grass:
+					Main = Color.Green;
+					Highlight = Color.Sienna;
+					Shadow = Color.SaddleBrown;
+					break;
+
+				case Tile.Stone:
+					Main = Color.FromArgb(128, 128, 128);
+					Highlight = Color.FromArgb(164, 164, 164);
+					Shadow = Color.FromArgb(100, 100, 100);
+					break;
+
+				case Tile.Cobblestone:
+					Main = Color.FromArgb(90, 90, 90);
+					Highlight = Color.FromArgb(124, 124, 124);
+					Shadow = Color.FromArgb(64, 64, 64);
+					break;
+
+				case Tile.Water:
+					Main = Color.FromArgb(0, 171, 236);
+					Highlight = Color.FromArgb(70, 204, 255);
+					Shadow = Color.FromArgb(0, 123, 170);
+					renderWater = true;
+					transparent = true;
+					break;
+
+				case Tile.StillWater:
+					Main = Color.FromArgb(0, 171, 236);
+					Highlight = Color.FromArgb(70, 204, 255);
+					Shadow = Color.FromArgb(0, 123, 170);
+					renderWater = true;
+					transparent = true;
+					break;
+
+				case Tile.Lava:
+					Main = Color.FromArgb(255, 125, 0);
+					Highlight = Color.FromArgb(255, 100, 0);
+					Shadow = Color.FromArgb(170, 50, 0);
+					break;
+
+				case Tile.StillLava:
+					Main = Color.FromArgb(236, 50, 0);
+					Highlight = Color.FromArgb(255, 100, 25);
+					Shadow = Color.FromArgb(170, 0, 0);
+					break;
+
+				/*case Tile.StillLava:
+				Main = Color.FromArgb(236, 171, 0);
+				Highlight = Color.FromArgb(255, 204, 70);
+				Shadow = Color.FromArgb(170, 123, 0);
+				break;*/
+
+				case Tile.Glass:
+					Main = Color.FromArgb(255, 255, 255);
+					Highlight = Color.FromArgb(255, 255, 255);
+					Shadow = Color.FromArgb(255, 255, 255);
+					renderGlass = true;
+					transparent = true;
+					break;
+
+				case Tile.SolidGold:
+					Main = Color.FromArgb(238, 237, 120);
+					Highlight = Color.FromArgb(238, 225, 64);
+					Shadow = Color.FromArgb(217, 202, 19);
+					break;
+
+				case Tile.Sand:
+					Main = Color.FromArgb(208, 206, 121);
+					Highlight = Color.FromArgb(217, 213, 157);
+					Shadow = Color.FromArgb(197, 190, 109);
+					break;
+
+				case Tile.Gravel:
+					Main = Color.FromArgb(158, 156, 71);
+					Highlight = Color.FromArgb(117, 113, 57);
+					Shadow = Color.FromArgb(97, 90, 09);
+					break;
+
+				case Tile.Red:
+					Main = Color.FromArgb(255, 0, 0);
+					Highlight = Color.FromArgb(255, 50, 50);
+					Shadow = Color.FromArgb(205, 0, 0);
+					break;
+
+				case Tile.Orange:
+					Main = Color.FromArgb(255, 128, 0);
+					Highlight = Color.FromArgb(255, 178, 50);
+					Shadow = Color.FromArgb(205, 78, 0);
+					break;
+
+				case Tile.Yellow:
+					Main = Color.FromArgb(255, 255, 0);
+					Highlight = Color.FromArgb(255, 255, 50);
+					Shadow = Color.FromArgb(205, 205, 0);
+					break;
+
+				case Tile.LightGreen:
+					Main = Color.FromArgb(128, 255, 0);
+					Highlight = Color.FromArgb(178, 255, 50);
+					Shadow = Color.FromArgb(78, 205, 0);
+					break;
+
+				case Tile.Green:
+					Main = Color.FromArgb(0, 255, 0);
+					Highlight = Color.FromArgb(50, 255, 50);
+					Shadow = Color.FromArgb(0, 205, 0);
+					break;
+
+				case Tile.AquaGreen:
+					Main = Color.FromArgb(0, 255, 128);
+					Highlight = Color.FromArgb(50, 255, 178);
+					Shadow = Color.FromArgb(0, 205, 78);
+					break;
+
+				case Tile.Cyan:
+					Main = Color.FromArgb(0, 128, 255);
+					Highlight = Color.FromArgb(50, 178, 255);
+					Shadow = Color.FromArgb(0, 78, 205);
+					break;
+
+				case Tile.Blue:
+					Main = Color.FromArgb(0, 0, 255);
+					Highlight = Color.FromArgb(50, 50, 205);
+					Shadow = Color.FromArgb(0, 0, 205);
+					break;
+
+				case Tile.Indigo:
+					Main = Color.FromArgb(128, 0, 255);
+					Highlight = Color.FromArgb(178, 50, 255);
+					Shadow = Color.FromArgb(78, 0, 205);
+					break;
+
+				case Tile.Purple:
+					Main = Color.FromArgb(255, 0, 255);
+					Highlight = Color.FromArgb(255, 50, 255);
+					Shadow = Color.FromArgb(205, 0, 205);
+					break;
+
+				case Tile.Pink:
+					Main = Color.FromArgb(255, 0, 128);
+					Highlight = Color.FromArgb(255, 50, 178);
+					Shadow = Color.FromArgb(205, 0, 78);
+					break;
+
+				case Tile.Bedrock:
+					Main = Color.FromArgb(50, 50, 50);
+					Highlight = Color.FromArgb(100, 100, 100);
+					Shadow = Color.FromArgb(25, 25, 25);
+					break;
+
+				case Tile.DarkGrey:
+					Main = Color.FromArgb(65, 65, 65);
+					Highlight = Color.FromArgb(115, 115, 115);
+					Shadow = Color.FromArgb(40, 40, 40);
+					break;
+
+				case Tile.LightGrey:
+					Main = Color.FromArgb(125, 125, 125);
+					Highlight = Color.FromArgb(175, 175, 175);
+					Shadow = Color.FromArgb(75, 75, 75);
+					break;
+
+				case Tile.White:
+					Main = Color.FromArgb(200, 200, 200);
+					Highlight = Color.FromArgb(255, 255, 255);
+					Shadow = Color.FromArgb(150, 150, 150);
+					break;
+
+				case Tile.Trunk:
+					Main = Color.FromArgb(147, 96, 0);
+					Highlight = Color.Sienna;
+					Shadow = Color.SaddleBrown;
+					break;
+
+				case Tile.Leaves:
+					Main = Color.FromArgb(0, 128, 0);
+					Highlight = Color.FromArgb(50, 178, 0);
+					Shadow = Color.FromArgb(0, 78, 0);
+					break;
+			}
+		}
+	}
+}
